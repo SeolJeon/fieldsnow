e83e0f95fdabba34883389dfe6798a56f8fc021b
SeolJeon <163636774+SeolJeon@users.noreply.github.com>
2024-09-24 16:16:36 +0900

    Update Hello World.c

--- Hello World.c
+++ Hello World.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int double(int n){
+  return n*2;
+}
 
 int main(){
+  int i;
+  
   printF("Hello World");
+  scanf("%d",&i);
+  printf("i: %d",i);
+
+  printf("double: %d",double(i));
   return 0;
 }
